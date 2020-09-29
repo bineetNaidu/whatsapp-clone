1e3750528a07f0b2398a953eacb800bb506480d1
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-29 20:30:04 +0530

    -- clean ups -- setting up client folder -- initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// STATICS
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Yo! Lets Build A MERN stack Whatsapp Clone.</h1>
+    </div>
+  );
+}
+
+export default App;
