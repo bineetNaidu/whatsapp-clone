4a3153529ca93dfaabab5ebd26592861f89e365f
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-30 14:53:03 +0530

    -- add pusher -- cors header && security

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const Pusher = require('pusher');
 
 // APP CONFIGS
 const app = express();
@@ -12,23 +13,50 @@ const app = express();
 const apiRoute = require('./routers');
 
 mongoose.Promise = global.Promise;
-mongoose
-  .connect(process.env.DB_CONNECTION, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    keepAlive: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MONGO_DB CONNECTED'))
-  .catch((err) => {
-    console.log(err.message);
-    process.exit(1);
+mongoose.connect(process.env.DB_CONNECTION, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  keepAlive: true,
+  useUnifiedTopology: true,
+});
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APPID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  // encrypted: true,
+  useTLS: true,
+});
+
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('DB watching');
+
+  const msgCollection = db.collection('messages');
+  const changeStream = msgCollection.watch();
+
+  changeStream.on('change', (change) => {
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument;
+      pusher.trigger('messages', 'inserted', {
+        name: messageDetails.name,
+        message: messageDetails.message,
+      });
+    } else {
+      console.log('Error Triggering Pusher');
+    }
   });
+});
 
 // MIDDLEWARES
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.XORIGIN,
+  })
+);
 app.use(helmet());
 
 // UNMOUNTING ROUTES
