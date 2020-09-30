4169cdf56238a38639eb95de74325e9e8cbe0922
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-30 16:44:57 +0530

    -- displaying mesgs -- add Moments -- post message -- add useForm Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
       {/* Chat component */}
     </div>
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -6,11 +6,27 @@ import AttachFileIcon from '@material-ui/icons/AttachFile';
 import IconButton from '@material-ui/core/IconButton';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
+import axios from './api';
+import useFormState from './hooks/useFormState';
+import Moment from 'react-moment';
 
 // STATICS
 import './Chat.css';
 
-const Chat = () => {
+const Chat = ({ messages }) => {
+  // HOOKS
+  const [msg, handleMsg, resetMsg] = useFormState('');
+  // functions
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    await axios.post('/new', {
+      name: 'Bineet',
+      message: msg,
+      received: true,
+    });
+    resetMsg();
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -35,30 +51,34 @@ const Chat = () => {
       </div>
 
       <div className="chat__body">
-        <p className="chat__message">
-          <span className="chat__name">User</span>
-          Message Form sender
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-
-        <p className="chat__message chat__receiver ">
-          <span className="chat__name">User</span>
-          Message Form sender
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-
-        <p className="chat__message">
-          <span className="chat__name">User</span>
-          Message Form sender
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
+        {messages.map(({ received, name, message, _id, timestamp }) => (
+          <p
+            className={`chat__message ${received && 'chat__receiver'}`}
+            key={_id}
+          >
+            <span className="chat__name">{name}</span>
+            {message}
+            <span className="chat__timestamp">
+              <Moment fromNow ago>
+                {timestamp}
+              </Moment>
+            </span>
+          </p>
+        ))}
       </div>
 
       <div className="chat__footer">
         <InsertEmoticonIcon />
         <form>
-          <input type="text" placeholder="Type a message " />
-          <button type="submit">Send a message</button>
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={msg}
+            onChange={handleMsg}
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
         </form>
         <MicIcon />
       </div>
